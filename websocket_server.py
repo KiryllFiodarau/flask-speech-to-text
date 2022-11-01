456adeae63ec3b87ec8aca84f0fcba7d161f2cdd
KiryllFiodarau <Kiryll.Fiodarau@itechart-group.com>
2022-11-01 18:31:43 +0300

    Changed file.

--- websocket_server.py
+++ websocket_server.py
@@ -14,9 +14,19 @@ os.environ["GOOGLE_CLOUD_PROJECT"] ='gcp-subtitles' #The GCP project ID
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] ='./gcp_config.json' #configuration file
 from google.auth import aws
 
-with open('gcp_config.json') as f:
-    json_config_info = json.load(f)
-print(json_config_info)
+json_config_info = {
+  "type": "external_account",
+  "audience": "//iam.googleapis.com/projects/337957535543/locations/global/workloadIdentityPools/awspool/providers/awsdata",
+  "subject_token_type": "urn:ietf:params:aws:token-type:aws4_request",
+  "token_url": "https://sts.googleapis.com/v1/token",
+  "credential_source": {
+    "environment_id": "aws1",
+    "region_url": "http://169.254.169.254/latest/meta-data/placement/availability-zone",
+    "url": "http://169.254.169.254/latest/meta-data/iam/security-credentials",
+    "regional_cred_verification_url": "https://sts.{region}.amazonaws.com?Action=GetCallerIdentity&Version=2011-06-15"
+  },
+  "service_account_impersonation_url": "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/aws-role@gcp-subtitles.iam.gserviceaccount.com:generateAccessToken"
+}
 credentials = aws.Credentials.from_info(json_config_info)
 print("Start init")
 client = speech.SpeechClient(credentials=credentials)
