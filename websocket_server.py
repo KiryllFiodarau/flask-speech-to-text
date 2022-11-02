afd42993aef901240bd0bb83504e2560e5b20679
KiryllFiodarau <Kiryll.Fiodarau@itechart-group.com>
2022-11-02 17:58:05 +0300

    Added simple data

--- websocket_server.py
+++ websocket_server.py
@@ -30,6 +30,23 @@ json_config_info = {
 credentials = aws.Credentials.from_info(json_config_info)
 print("Start init")
 client = speech.SpeechClient(credentials=credentials)
+print("Finish init 0")
+
+gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+
+audio = speech.RecognitionAudio(uri=gcs_uri)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
+
+# Detects speech in the audio file
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    print("Transcript: {}".format(result.alternatives[0].transcript))
 print("Finish init")
 print("----------------------------------------------------")
 
